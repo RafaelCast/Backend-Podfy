fc846848c9cf8a6aef60b971c1ff2c0db4d277ce
Luiz Ricardo Brumati De Lima <luiz.1303@hotmail.com>
2022-05-30 13:41:33 -0300

    Adicionado primeiras alterações fronted

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from kivy.app import App
+from kivy.config import Config
+# Config.set('graphics', 'resizable', False) #Redimensionamento de janela
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
+
+Window.size = (360, 800)
 
 GUI = Builder.load_file("cadastro.kv")
 
@@ -13,7 +19,7 @@ class User:
         self.senha = senha
 
 
-class MyGrid(Widget):
+class LoginScreen(Widget):
     email = ObjectProperty(None)
     confirm = ObjectProperty(None)
     cpf = ObjectProperty(None)
@@ -50,9 +56,20 @@ class MyGrid(Widget):
 
 
 class MyApp(App):
+    
+    podfy_logo_white = os.path.join ('resources', 'podfy-logo-whitebg.png') #Associa a logo com fundo branco com a variável podfy_logo_white
+
+    #Associa as fontes com variáveis, para facilitar o uso posterior:
+    inter_regular = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-Regular.ttf')
+    inter_medium = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-Medium.ttf')
+    inter_semibold = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-SemiBold.ttf')
+    inter_bold = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-Bold.ttf')
+    
     def build(self):
-        return MyGrid()
+        Window.clearcolor = (1,1,1,1) #Define o background branco
+        return LoginScreen()
 
 
+#Só vai rodar a aplicação se este arquivo (main.py) for executado individualmente (Ou seja, dando um "python main.py"). Se for importado e executado através de outro arquivo, não vai executar o conteúdo da função abaixo!
 if __name__ == '__main__':
     MyApp().run()
