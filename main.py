1a6d305db5d36ef4cfe928999d6a7961da4d7f00
Luiz Ricardo Brumati De Lima <luiz.1303@hotmail.com>
2022-06-10 18:50:39 -0300

    Front de login, cadastro e separação de arquivos

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import os
-from kivy.app import App
-from kivy.config import Config
-# Config.set('graphics', 'resizable', False) #Redimensionamento de janela
-from kivy.lang import Builder
-from kivy.properties import ObjectProperty
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import ButtonBehavior
-
-Window.size = (360, 800)
-
-class WindowManager (ScreenManager):
-    #Classe Root que vai lidar com transição de Screens
-    pass
+from utils import * #importa todas as classes do arquivo classController.py
 
 class User:
     def __init__(self, email, cpf, senha):
@@ -42,8 +27,6 @@ class RegisterScreen(Screen):
         elif len(self.cpf.text) != 11:
             print("CPF inválido")
 
-
-
     def val(self):
             if self.email.text == self.confirm.text and len(self.cpf.text) == 11:
                 user = User(self.email.text, self.cpf.text, self.senha.text)
@@ -56,23 +39,27 @@ class RegisterScreen(Screen):
                 print("CPF inválido")
 
 
-class LoginScreen(Screen):
-    pass
-
+GUI = Builder.load_file("screen_manager.kv")
 GUI = Builder.load_file("cadastro.kv")
+GUI = Builder.load_file("customizacao.kv")
+
 class Podfy(App):
 
-    # podfy_logo_white = os.path.join ('resources', 'podfy-logo-whitebg.png') #Associa a logo com fundo branco com a variável podfy_logo_white
-    podfy_logo_white = os.path.join ('resources', 'podfy-logo-whitebg.jpg') #Associa a logo com fundo branco com a variável podfy_logo_white
-
-    #Associa as fontes com variáveis, para facilitar o uso posterior:
+    #Importação de recursos
+    podfy_logo_white = os.path.join ('resources', 'podfy-logo-whitebg.jpg')
     inter_regular = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-Regular.ttf')
     inter_medium = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-Medium.ttf')
     inter_semibold = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-SemiBold.ttf')
     inter_bold = os.path.join('resources', 'fontes', 'Inter', 'static', 'Inter-Bold.ttf')
-    
+
+    user_icon = os.path.join('resources','img','profile_icon.png')
+    user_icon_hover = os.path.join('resources','img','profile_icon_hover.png')
+
     def build(self):
-        return WindowManager()
+        sm = WindowManager()
+        # sm.current = 'login_screen'
+        sm.current = 'login_screen' #Tela padrão que o WindowManager vai iniciar
+        return sm
 
 
 #Só vai rodar a aplicação se este arquivo (main.py) for executado individualmente (Ou seja, dando um "python main.py"). Se for importado e executado através de outro arquivo, não vai executar o conteúdo da função abaixo!
